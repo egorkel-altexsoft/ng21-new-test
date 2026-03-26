ef2b19cbe073c00ae43e8491c37a1dd9e0c0929c
Yehor Kolesnykov <yehor.kolesnykov@altexsoft.com>
2026-03-26 19:13:46 +0100

    feat: branded types test

--- src/app/movie/movie.schema.ts
+++ src/app/movie/movie.schema.ts
@@ -4,7 +4,7 @@ export const MovieSchema = z.object({
   Title: z.string(),
   Year: z.string(),
   Poster: z.string(),
-  imdbID: z.string()
+  imdbID: z.string().brand<'ImdbId'>()
 });
 
 export type Movie = z.infer<typeof MovieSchema>;
@@ -16,3 +16,35 @@ export const MovieSearchResultSchema = z.object({
 });
 
 export type MovieSearchResult = z.infer<typeof MovieSearchResultSchema>;
+
+// Tests with branded types
+const commonDbSchema = z.object({
+  id: z.string().brand<'DbId'>(),
+  createdAt: z.date(),
+  updatedAt: z.date()
+});
+
+const userDbSchema = commonDbSchema.extend({
+  name: z.string(),
+  email: z.email(),
+  id: commonDbSchema.shape.id.brand<'UserId'>()
+});
+type UserDb = z.infer<typeof userDbSchema>;
+
+const user = userDbSchema.parse({
+  id: '123',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  name: 'John Doe',
+  email: 'john.doe@example.com'
+});
+
+function deleteUser(_userId: UserDb['id']) {
+  // ...
+}
+
+// @ts-expect-error - Simple strings are not allowed
+deleteUser('123');
+
+// No error
+deleteUser(user.id);
